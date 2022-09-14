ff296e573c9c999eab2f8ee006e3748bf4f8d2c7
Reja Jamil <reja.copyright@gmail.com>
2022-09-14 18:50:32 +0700

    ci: check host

--- _api/config.jsx
+++ _api/config.jsx
@@ -1,1 +1,9 @@
-export const api = 'http://localhost/kss-api'
+let api = 'http://localhost/kss-api'
+if (typeof window !== 'undefined') {
+  console.log(window?.location)
+  if (window?.location?.hostname !== 'localhost') {
+    api = 'https://api.krakatausamuderasolusi.co.id'
+  }
+}
+
+export { api }
