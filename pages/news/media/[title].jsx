81605a8ca6963ec61018dbc9180dc6c7bc19bf82
Reja Jamil <reja.copyright@gmail.com>
2022-09-18 18:17:10 +0700

    feat: news loader

--- pages/news/media/[title].jsx
+++ pages/news/media/[title].jsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/router'
 import Banner from '@components/banner'
 import { detailMedia } from '@api/news'
 import moment from 'moment'
+import { CardLoader, TextLoader } from '@components/loader'
 
 const Breadcrumb = () => {
   return (
@@ -33,11 +34,17 @@ const Index = () => {
   const { query } = useRouter()
   const { id } = query || {}
   const [detail, setDetail] = useState(undefined)
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
     if (id) {
-      detailMedia(id).then(({ data }) => {
-        setDetail(data)
-      })
+      setLoading(true)
+      detailMedia(id)
+        .then(({ data }) => {
+          setDetail(data)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
     }
   }, [id])
   return (
@@ -50,55 +57,68 @@ const Index = () => {
             <Breadcrumb />
           </div>
         </div>
-        {detail ? (
-          <div className='row'>
-            <div className='col-12 mb-5'>
-              {!!detail?.file && (
-                <div className='position-relative h-300px bg-white overflow-hidden radius-10 shadow-lg-bold mb-3'>
-                  <Image
-                    priority
-                    quality={30}
-                    alt='img'
-                    layout='fill'
-                    objectFit='cover'
-                    src={detail?.file}
-                  />
-                </div>
-              )}
-              <div className='flex-end'>
-                <i className='las la-user me-1 fs-6 text-primary' />
-                <div className='text-primary'>Uploaded at</div>
-                <div className='text-aa mx-2'>-</div>
-                <div className='text-aa'>
-                  {moment(detail?.created_at || undefined)?.format('DD MMMM Y, H:mm A')}
-                </div>
-                <i className='las la-clock ms-1 fs-7 text-aa' />
-              </div>
-            </div>
-            <div className='col-12'>
-              <div className='fs-4 fw-500 mb-3 text-capitalize'>{detail?.title}</div>
-              <div className='w-100' dangerouslySetInnerHTML={{ __html: detail?.description }} />
-            </div>
+        {loading ? (
+          <div className='mt-2'>
+            <CardLoader count={1} className='col-12 mb-5' height={200} />
+            <TextLoader count={3} className='col-12 mb-3' />
           </div>
         ) : (
-          <div className='flex-center vh-50 mb-5'>
-            <div className='text-center'>
-              <div className='position-relative mx-auto mb-3 same-200px'>
-                <Image
-                  priority
-                  style={{ opacity: 0.5 }}
-                  alt='img'
-                  layout='fill'
-                  objectFit='contain'
-                  quality={50}
-                  src={require(`@images/stock/15-dark.png`)}
-                />
+          <>
+            {detail ? (
+              <div className='row'>
+                <div className='col-12 mb-5'>
+                  {!!detail?.file && (
+                    <div className='position-relative h-300px bg-white overflow-hidden radius-10 shadow-lg-bold mb-3'>
+                      <Image
+                        priority
+                        quality={30}
+                        alt='img'
+                        layout='fill'
+                        objectFit='cover'
+                        src={detail?.file}
+                      />
+                    </div>
+                  )}
+                  <div className='flex-end'>
+                    <i className='las la-user me-1 fs-6 text-primary' />
+                    <div className='text-primary'>Uploaded at</div>
+                    <div className='text-aa mx-2'>-</div>
+                    <div className='text-aa'>
+                      {moment(detail?.created_at || undefined)?.format('DD MMMM Y, H:mm A')}
+                    </div>
+                    <i className='las la-clock ms-1 fs-7 text-aa' />
+                  </div>
+                </div>
+                <div className='col-12'>
+                  <div className='fs-4 fw-500 mb-3 text-capitalize'>{detail?.title}</div>
+                  <div
+                    className='w-100'
+                    dangerouslySetInnerHTML={{ __html: detail?.description }}
+                  />
+                </div>
               </div>
-              <div className='fw-300 fs-6'>
-                We&apos;re sorry, but We couldn&apos;t find <br /> the data you&apos;re looking for
+            ) : (
+              <div className='flex-center vh-50 mb-5'>
+                <div className='text-center'>
+                  <div className='position-relative mx-auto mb-3 same-200px'>
+                    <Image
+                      priority
+                      style={{ opacity: 0.5 }}
+                      alt='img'
+                      layout='fill'
+                      objectFit='contain'
+                      quality={50}
+                      src={require(`@images/stock/15-dark.png`)}
+                    />
+                  </div>
+                  <div className='fw-300 fs-6'>
+                    We&apos;re sorry, but We couldn&apos;t find <br /> the data you&apos;re looking
+                    for
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            )}
+          </>
         )}
       </div>
     </>
--- pages/news/media/index.jsx
+++ pages/news/media/index.jsx
@@ -6,48 +6,59 @@ import Meta from '@components/seo/meta'
 import { getMedia } from '@api/news'
 import defaultImage from '@images/placeholder-image.jpg'
 import { htmlToString, strToSlug } from '@helpers'
+import { CardLoader } from '@components/loader'
 
 const Index = () => {
   const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
-    getMedia().then(({ data: { data } = {} }) => {
-      if (data?.length) {
-        setData(data)
-      }
-    })
+    setLoading(true)
+    getMedia()
+      .then(({ data: { data } = {} }) => {
+        setData(data || [])
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }, [])
   return (
     <>
       <Meta title='Media' description={undefined} />
-      <div className='row'>
-        {data.map(({ id, title, description, file }, index) => (
-          <div key={index} className='col-sm-6 col-lg-4 mb-5 flex-column-reverse'>
-            <div className='position-relative radius-10 overflow-hidden bg-white shadow-sm hover-lg-bold h-100'>
-              <div className='position-relative h-150px bg-white overflow-hidden'>
-                <Image
-                  priority
-                  quality={30}
-                  alt='img'
-                  layout='fill'
-                  objectFit='cover'
-                  src={file || defaultImage}
-                />
-              </div>
-              <div className='p-3 w-100 mb-5'>
-                <div className='fs-8 fw-500 mb-2 text-truncate-2'>{title}</div>
-                <div className='fs-9 fw-300 text-bb text-truncate-3'>
-                  {htmlToString(description)}
+      {loading ? (
+        <div className='mt-2'>
+          <CardLoader count={6} className='col-sm-6 col-lg-4 mb-4' />
+        </div>
+      ) : (
+        <div className='row mt-2'>
+          {data.map(({ id, title, description, file }, index) => (
+            <div key={index} className='col-sm-6 col-lg-4 mb-5 flex-column-reverse'>
+              <div className='position-relative radius-10 overflow-hidden bg-white shadow-sm hover-lg-bold h-100'>
+                <div className='position-relative h-150px bg-white overflow-hidden'>
+                  <Image
+                    priority
+                    quality={30}
+                    alt='img'
+                    layout='fill'
+                    objectFit='cover'
+                    src={file || defaultImage}
+                  />
+                </div>
+                <div className='p-3 w-100 mb-5'>
+                  <div className='fs-8 fw-500 mb-2 text-truncate-2'>{title}</div>
+                  <div className='fs-9 fw-300 text-bb text-truncate-3'>
+                    {htmlToString(description)}
+                  </div>
+                </div>
+                <div className='absolute-center-h bottom-0 mb-3'>
+                  <Link href={`/news/media/${strToSlug(title)}?id=${id}`}>
+                    <div className='btn btn-sm btn-light-primary fs-8'>Read more</div>
+                  </Link>
                 </div>
               </div>
-              <div className='absolute-center-h bottom-0 mb-3'>
-                <Link href={`/news/media/${strToSlug(title)}?id=${id}`}>
-                  <div className='btn btn-sm btn-light-primary fs-8'>Read more</div>
-                </Link>
-              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </>
   )
 }
